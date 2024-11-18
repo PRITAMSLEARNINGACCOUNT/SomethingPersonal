d69d4585ad3e5c586c77fc834eb136e866141ba6
PRITAM SAHA <pritamstech@hotmail.com>
2024-11-19 00:20:15 +0530

    Let's Go With The Real Experiment

--- src/app/api/compile/route.ts
+++ src/app/api/compile/route.ts
@@ -1,17 +1,25 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // import { exec } from "child_process";
 import path from "path";
 import fs from "fs";
+import { exec } from "child_process";
 
-export async function GET() {
+export async function POST(Request: NextRequest) {
   try {
+    let { Code } = await Request.json();
     // Create a temporary file in the /tmp directory
     const tempFilePath = path.join("/tmp", "temp.c");
 
     // Write the C code to the temporary file
-    fs.writeFileSync(tempFilePath, "Hello World");
-    return NextResponse.json({ message: "Hello World" });
+    fs.writeFileSync(tempFilePath, Code);
+    exec(`gcc ${tempFilePath} -o /tmp/temp && /tmp/temp`, (error, stdout) => {
+      if (error instanceof Error) {
+        console.error(error.message);
+        return NextResponse.json({ message: error.message });
+      }
+      return NextResponse.json({ message: stdout.toString() });
+    });
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.message);
